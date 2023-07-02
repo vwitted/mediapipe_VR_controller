c4ebb28d222b384b91e5c5d82457ea5b277be068
phil <138331392+vwitted@users.noreply.github.com>
2023-07-02 23:48:46 +0100

    Update vr_controller.py
    added comments

--- vr_controller.py
+++ vr_controller.py
@@ -1,14 +1,19 @@
 import cv2
 import mediapipe as mp
 from pythonosc.udp_client import SimpleUDPClient
+
+#set up variables for OSC Client
+#values determined by hard-coded Steam driver 'apritags'
 ip = "127.0.0.1"
 port = 39570
 client = SimpleUDPClient(ip, port)
+
+#set up mediapipe utils
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# For webcam input:
+#webcam input capture:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
     model_complexity=0,
@@ -32,8 +37,10 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
+        #this simple version uses only one hand landamark, the wrist to set whole hand position
         client.send_message("/VMT/Raw/Unity", [1, 5, 0., 0.5-hand_landmarks.landmark[0].x,0.5-hand_landmarks.landmark[0].y,1-hand_landmarks.landmark[0].z,0.,0.,0.,0.])
 
+        #landmarks are redrawn on camera preview  
         mp_drawing.draw_landmarks(
             image,
             hand_landmarks,
